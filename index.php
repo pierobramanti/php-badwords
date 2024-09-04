fdac7f660ee15b2efa588862ed553fe2d3d44f3a
pierobramanti <piero.bramanti94@gmail.com>
2024-09-04 16:48:29 +0200

    [UPD] created input section

--- index.php
+++ index.php
@@ -1,16 +1,41 @@
+<?php
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Bad Words</title>
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                
+            <div class="col-12 border">
+                <form action="" method="GET">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="form-group">
+                                <div class="control-label">
+                                    Digita Qualcosa
+                                </div>
+                                <input type="text" class="form-control" name="" placeholder="Digita Qualcosa" >
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="form-group">
+                                <div class="control-label">
+                                    Digita Qualcosa
+                                </div>
+                                <input type="text" class="form-control" name="" placeholder="Digita Qualcosa" >
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
